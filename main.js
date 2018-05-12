2b691a3cfcb2342810fbf4819f8524cb76b55a1f
Peter Fernandes <supersonicandtails@gmail.com>
2018-05-12 15:08:44 -0400

    Navbar wireframe in place

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onscroll = (evt) => {
 	const title = document.querySelector(".page-title");
+	const navbar = document.querySelector(".navbar");
 
 	if (window.pageYOffset >= window.innerHeight * .4)
 		title.classList.add("page-title--docked");
@@ -10,4 +11,14 @@ window.onscroll = (evt) => {
 		title.classList.add("page-title--small");
 	else
 		title.classList.remove("page-title--small");
+	
+	if (window.pageYOffset >= window.innerHeight)
+		navbar.classList.remove("navbar--hidden");
+	else
+		navbar.classList.add("navbar--hidden");
 }
+
+const firstName = document.querySelector(".page-title--firstname");
+firstName.onclick = evt => {
+	window.scrollTo(0, 0);
+};
