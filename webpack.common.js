289f503817c9c4bab027d4682660bf3c1d10cb2a
Peter Fernandes <supersonicandtails@gmail.com>
2018-05-20 10:16:07 -0400

    Initialize webpack

---
+++ webpack.common.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	entry: {
+		"site": "./main.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: [{
+						loader: "css-loader",
+						options: {
+							minimize: true
+						}
+					}]
+				})
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "images/[name].[ext]"
+						}
+					}
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js' ]
+	},
+	output: {
+		filename: "[name]/bundle.js",
+		publicPath: "/static/",
+		path: path.resolve(__dirname, "./dist/")
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./index.html",
+			inject: "body",
+			filename: "index.html",
+			chunks: ["site"]
+		}),
+		new ExtractTextPlugin({
+			filename: "styles.css"
+		}),
+		// Ignore all locale files of moment.js
+		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+	]
+};
